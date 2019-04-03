20377a6cc053d609e220d3d63b9e20e187c30d7b
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-04-03 06:50:21 -0700

    update gitignore

--- src/strings/Strings.java
+++ src/strings/Strings.java
@@ -1,20 +1,48 @@
 package strings;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Strings {
 
 	public static void main(String[] args) {
-		String magic = "Magic";
-		char letter = 'b';
-		System.out.println(hasLetter(magic, letter));
+//		String magic = "Magic";
+//		char letter = 'b';
+//		System.out.println(hasLetter(magic, letter));
+//		
+//		String replaced = replace(magic, 'a', 'd');
+//		System.out.println(replaced);
+//		
+//		String regexCheck = "123.334.5999.579";
+//		String[] values = regexCheck.split("\\.");
+//		for (String v : values) {
+//			System.out.println(v);
+//		}
+//		
+		String testing = "1234|5678|910";
+		String[] splits = testing.split("\\|");
+		System.out.println(splits[0]);
 		
-		String replaced = replace(magic, 'a', 'd');
-		System.out.println(replaced);
+		Set<String> enroll = new HashSet<String>();
+		Set<String> unenroll = new HashSet<String>();
+		enroll.add("Hello");
+		enroll.add("Yup");
+		enroll.add("OK!");
+		unenroll.add("OK!");
+		unenroll.add("Nope");
 		
-		String regexCheck = "123.334.5999.579";
-		String[] values = regexCheck.split("\\.");
-		for (String v : values) {
-			System.out.println(v);
+		//expect Hello Yup
+		enroll.removeAll(unenroll);
+		for (String x : enroll) {
+			System.out.println("enroll " + x);
 		}
+		
+		unenroll.removeAll(enroll);
+		for (String x : unenroll) {
+			System.out.println("unenroll " + x);
+		}
+		
+		
 	}
 	
 	public static int hasLetter(String word, char letter) {
