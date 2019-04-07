a752e1db63d997e7d91d77ec2b530b980f1dd2f3
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-04-07 14:04:36 -0700

    compare binary search tree and linked list performance

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -12,6 +12,9 @@ public class DictionaryBST implements Dictionary
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
+   public DictionaryBST() {
+	   this.dict = new TreeSet<String>();
+   }
 	
     
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,22 +23,28 @@ public class DictionaryBST implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    	if (isWord(word)) {
+    		return false;
+    	} else {
+    		this.dict.add(word.toLowerCase());
+    		return true;
+    	}
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-    	// TODO: Implement this method
-        return 0;
+    	return this.dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
+    	if (this.dict.contains(s.toLowerCase())) {
+    		return true;
+    	} else {
+			return false;
+    	}
     }
 
 }
--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -12,6 +12,9 @@ public class DictionaryLL implements Dictionary
 	private LinkedList<String> dict;
 	
     // TODO: Add a constructor
+	public DictionaryLL() {
+		this.dict = new LinkedList<String>();
+	}
 
 
     /** Add this word to the dictionary.  Convert it to lowercase first
@@ -20,23 +23,26 @@ public class DictionaryLL implements Dictionary
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+    	if(isWord(word)) {
+    		return false;
+    	} else {
+    		this.dict.add(word.toLowerCase());
+    		return true;
+    	}
     }
-
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-        // TODO: Implement this method
-        return 0;
+    	return this.dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
+    	if (this.dict.contains(s.toLowerCase())) {
+    		return true;
+    	} else {
+    		return false;
+    	}
     }
-
-    
 }
